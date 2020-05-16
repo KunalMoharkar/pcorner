8194205e8cc42d6003b6da4c6f77e7547c5dad56
Kunal Moharkar <kunalmoharkar1700@gmail.com>
2020-05-16 22:33:33 +0530

    post routes

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,16 +2,22 @@ from rest_framework import serializers
 from django.conf import settings
 from .models import Professor
 from django.contrib.auth import get_user_model
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
         fields = ['id','username','email','is_teacher','is_student']
+        extra_kwargs = {
+            'username':{
+                'validators':[UnicodeUsernameValidator()],
+            }
+        }
 
 class ProfessorSerializer(serializers.ModelSerializer):
 
-    user = UserSerializer(read_only = True)
+    user = UserSerializer()
 
     class Meta:
         model = Professor
--- projects/serializers.py
+++ projects/serializers.py
@@ -4,27 +4,29 @@ from accounts.models import Professor
 from django.contrib.auth.models import User
 from django.conf import settings
 from accounts.serializers import UserSerializer,ProfessorSerializer
+from django.contrib.auth import get_user_model
 
 
 class ProjectSerializer(serializers.ModelSerializer):
 
-    professor = ProfessorSerializer(read_only = True)
+    professor = ProfessorSerializer(read_only=True)
+    professor_id = serializers.PrimaryKeyRelatedField(queryset = Professor.objects.all(), source='professor', write_only=True)
+
 
     class Meta:
         model = Project
         fields= '__all__'
 
-    def create(self, validated_data):
-        professor_data = validated_data.pop('professor')
-        professor = Professor.objects.create(**validated_data)
-        Project.objects.create(professor=professor, **professor_data)
-        return professor
+
+
 
 
 
 class ApplicationSerializer(serializers.ModelSerializer):
 
-    project = ProjectSerializer(read_only = True)
+    project = ProjectSerializer(read_only=True)
+    project_id = serializers.PrimaryKeyRelatedField(queryset = Project.objects.all(), source='project', write_only=True)
+
 
     class Meta:
         model = Application
